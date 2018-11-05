0fa229e11976482b6ef434e9bf01daa007ca5f11
dany corbineau <danycor2@hotmail.fr>
2018-11-05 16:51:31 +0100

    tentative tocken

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path    = require("path");
 const app = express();
 var bodyParser = require('body-parser');
-
+var jwt = require('jsonwebtoken');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -15,9 +15,21 @@ var objectsValue = [
     {path:'/list/', name:'List', fields: ['id', 'name', 'user', 'items']}
 ];
 
+var algo = {
+    "alg": "HS512",
+    "typ": "JWT"
+  };
+var payload = {
+    "sub": "danyRight",
+    "name": "dany",
+    "admin": true, 
+    "iat": 1541431715
+  };
 
-app.use('*items/create*', function (req, res, next) {console.log('Create new Items');next();});
-app.use('*list/create*', function (req, res, next) {console.log('Create new List');next();});
+  var password = "ctyuslrbimèy__zanovgy_lqsn_vlncgudtwmvv!buglicbf:ugfnk;ukwtgvynksnob:divhngy;fsxty,gdxj:gbuhlcdgbnfg:u";
+  var code = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.DuN8Xs4-qXJLTsQZvdBg-yl5d8szlySO0ALkAH930tQIe9_4WEDe6BadvRYAe61vB5C3ybDoyFLUYcJBum_7Zg";
+    
+  
 
 function saveFile(objectName, content, returnValue, res){
     fs.writeFile(objectName+'.json', JSON.stringify(content), (err) => {  
@@ -28,12 +40,13 @@ function saveFile(objectName, content, returnValue, res){
     });
 }
 
+function writeLog(mode, name, uid){
+    fs.writeFile('log.log', new Date()+' - '+mode+' '+name+' by '+uid+'\n\r', { flag: 'a+' }, (err) => {});
+}
+
 objectsValue.forEach(objectvalue => {
 
-    app.use(objectvalue.path+'create', function (req, res, next) {console.log(new Date()+' - Create new '+objectvalue.name);next();});
-    app.use(objectvalue.path+'delete', function (req, res, next) {console.log(new Date()+' - Delete '+objectvalue.name);next();});
-    app.use(objectvalue.path+'update', function (req, res, next) {console.log(new Date()+' - Update '+objectvalue.name);next();});
-    app.use(objectvalue.path, function (req, res, next) {console.log(new Date()+' - Shwo list of '+objectvalue.name);next();});
+   
 
     app.get(objectvalue.path, function (req, res) {
         var content = fs.readFileSync(objectvalue.name+".json");
@@ -41,6 +54,11 @@ objectsValue.forEach(objectvalue => {
     
         return res.send(jsonContent);
     });
+
+    app.use(objectvalue.path+'create', function (req, res, next){console.log("log:"+req.sessionID);writeLog('Create new', objectvalue.name, req.sessionID);next();});
+    app.use(objectvalue.path+'delete', function (req, res, next) {writeLog('Delete', objectvalue.name, req.sessionID);next();});
+    app.use(objectvalue.path+'update', function (req, res, next) {writeLog('Update', objectvalue.name, req.sessionID);next();});
+    app.use(objectvalue.path, function (req, res, next) {writeLog('Show list of', objectvalue.name, req.sessionID);next();});
     
     app.post(objectvalue.path, function (req, res) {
         var content = fs.readFileSync(objectvalue.name+".json");
@@ -50,6 +68,12 @@ objectsValue.forEach(objectvalue => {
     });
 
     app.post(objectvalue.path+'create', function (req, res) {
+        //console.log(req.sessionID);
+        var codeGenerated = HMACSHA512(
+            base64UrlEncode(algo) + "." +
+            base64UrlEncode(payload),password)
+        console.log(codeGenerated);
+
         var creatObject={};
         objectvalue.fields.forEach(field => {
             creatObject[field] = req.body[field];
@@ -107,8 +131,6 @@ objectsValue.forEach(objectvalue => {
 app.get('/', function (req, res) {
     return res.send('/user/ | /items/ | /list/');
 });
-
-
 
 app.listen(9999, () => {
     console.log('Start at 9999');
